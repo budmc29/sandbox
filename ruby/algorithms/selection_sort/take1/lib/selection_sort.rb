0926e200c206ae806957d52c9138566938f5c675
mugurel chirica <chirica.mugurel@gmail.com>
2017-01-02 03:24:36 +0000

    add alternative_sort (implementation by someone else)

--- ruby/algorithms/selection_sort/take1/lib/selection_sort.rb
+++ ruby/algorithms/selection_sort/take1/lib/selection_sort.rb
@@ -1,4 +1,6 @@
 class SelectionSort
+  @debug = false
+
   def initialize(data)
     @data = data.dup
   end
@@ -6,7 +8,8 @@ class SelectionSort
   def run
     verify
 
-    sort
+    # sort
+    alternative_sort
   end
 
   private
@@ -20,37 +23,33 @@ class SelectionSort
   end
 
   def sort
-    sort_explained
-  end
+    puts "starting array #@data" if @debug
 
-  def sort_explained
     sort_max = @data.size
-    puts "starting array #@data"
-
     sort_max.times do |i|
       smallest_position = i
 
       j = i + 1
-      (sort_max - 1).times do |new_index|
+      (sort_max).times do
         current_item = @data[smallest_position]
         last_read = @data[j]
 
         if !last_read.nil?
-          puts "comparing #{last_read} with #{current_item}"
+          puts "comparing #{last_read} with #{current_item}" if @debug
           if (last_read < current_item)
-            puts "#{last_read} is smaller than #{current_item}, update smallest item position #{j}: #{@data[j]}"
+            puts "#{last_read} is smaller than #{current_item}, update smallest item position #{j}: #{@data[j]}" if @debug
 
             smallest_position = j
           else
-            puts "#{last_read} is bigger, ignore it"
+            puts "#{last_read} is bigger, ignore it" if @debug
           end
         end
 
         j += 1
       end
 
-      if smallest_position != i
-        puts "smallest item position changed, swaping order"
+      unless smallest_position == i
+        puts "smallest item position changed, swaping order" if @debug
 
         @old_data = @data.dup
 
@@ -59,10 +58,32 @@ class SelectionSort
         @data[i] = temp
       end
 
-      puts "old_data #@old_data"
-      puts "data #@data"
+      if @debug
+        puts "old_data #@old_data"
+        puts "data #@data"
+      end
     end
 
+    @data
+  end
+
+  def alternative_sort
+    # Selection sort: A naive sort that goes through the container and selects the smallest element,
+    # putting it at the beginning. Repeat until the end is reached.
+    # Requirements: Needs to be able to compare elements with <=>, and the [] []= methods should
+    # be implemented for the container.
+    # Time Complexity: О(n^2)
+    # Space Complexity: О(n) total, O(1) auxiliary
+    # Stable: Yes
+    #
+    #   Algorithms::Sort.selection_sort [5, 4, 3, 1, 2] => [1, 2, 3, 4, 5]
+    0.upto(@data.size-1) do |i|
+      min = i
+      (i+1).upto(@data.size-1) do |j|
+        min = j if (@data[j] <=> @data[min]) == -1
+      end
+      @data[i], @data[min] = @data[min], @data[i] # Swap
+    end
     @data
   end
 end
