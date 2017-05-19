7eca188801b19a2af224cb58a2c6645d7c8b9eff
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2017-05-19 01:25:49 +0100

    Java: Real input

---
+++ java/bucket/ReadString.java
@@ -0,0 +1,27 @@
+package bucket;
+
+import java.io.*;
+
+public class ReadString
+{
+    public static void main(String[] args) {
+        System.out.print("Enter string: ");
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader( isr );
+
+        String input = "";
+
+        try
+        {
+            input = buffer.readLine();
+            buffer.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("An input error has occurred.");
+        }
+
+        System.out.println("Your input is: " + input);
+    }
+}
