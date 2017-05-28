953d90871ee64df5421db2b1c97a26968684ba4a
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2017-05-28 01:38:29 +0100

    Set actions for spinner

--- Android/BeerAdviser/app/src/main/java/budmc29/beeradviser/FindBeerActivity.java
+++ Android/BeerAdviser/app/src/main/java/budmc29/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package budmc29.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById( R.id.brands );
+
+        Spinner colour = (Spinner) findViewById( R.id.colour );
+
+        String beerType = String.valueOf( colour.getSelectedItem() );
+
+        brands.setText( beerType );
     }
 }
