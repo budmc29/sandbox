afd905fdb827dc57c87f7e7a923350f503a0290a
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2017-06-02 01:42:06 +0100

    Create jar file for Lotto

--- java/bucket/Lotto.java
+++ java/bucket/Lotto.java
@@ -2,12 +2,7 @@ package bucket;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class Lotto extends JFrame implements ActionListener {
 
