a9143454c2ea8e21b31642a8694891ac9ab13b78
budmc29 <chirica.mugurel@gmail.com>
2017-07-02 13:43:47 +0100

    Refactor algorithm to make it more readable

--- ruby/algorithms/bubble_sort/lib/bubble_sort.rb
+++ ruby/algorithms/bubble_sort/lib/bubble_sort.rb
@@ -5,28 +5,40 @@
 # which indicates that the list is sorted.
 class BubbleSort
   def initialize(array)
-    @array = array
+    @list = array.dup
   end
 
   def call
-    validate(@array)
-
-    required_passes = @array.size - 1
-    required_passes.times do
-      @array.each_with_index do |e, i|
-        if (@array[i] > @array[i + 1] rescue false)
-          @array[i], @array[i + 1] = @array[i + 1], @array[i]
-        end
-      end
-    end
-
-    @array
+    validate(@list)
+    sort
   end
 
   private
 
+  def sort
+    pases = @list.size - 1
+    pases.times do
+      @list.each_with_index do |e, i|
+        _current = i
+        _next = _current + 1
+
+        next if @list[_next].nil?
+
+        if @list[_current] > @list[_next]
+          swap_values(_current, _next)
+        end
+      end
+    end
+
+    @list
+  end
+
   def validate(input)
     raise TypeError unless input.respond_to?(:each)
     raise TypeError unless input.all?{ |val| val.is_a?(Integer) }
+  end
+
+  def swap_values(_current, _next)
+    @list[_current], @list[_next] = @list[_next], @list[_current]
   end
 end
