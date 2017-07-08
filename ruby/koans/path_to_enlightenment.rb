07ace6f7230708ef9e227b92d8565dd19503d3d9
budmc29 <chirica.mugurel@gmail.com>
2017-07-08 15:05:53 +0100

    Koans: About Regular Experssions

--- ruby/koans/path_to_enlightenment.rb
+++ ruby/koans/path_to_enlightenment.rb
@@ -10,8 +10,8 @@ require 'about_array_assignment'
 require 'about_hashes'
 require 'about_strings'
 require 'about_symbols'
-
 require 'about_regular_expressions'
+
 require 'about_methods'
 in_ruby_version("2") do
   require 'about_keyword_arguments'
