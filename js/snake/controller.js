7a309436145872624d3bfe5d028dc3631a8b93c7
budmc29 <chirica.mugurel@gmail.com>
2017-10-09 22:45:10 +0100

    [JS] Snake: Initial

---
+++ js/snake/controller.js
@@ -0,0 +1,9 @@
+document.onkeydown = function(e) {
+  var keys = {
+  37: 'left',
+  38: 'up'
+  39: 'rigth'
+  40: 'down'
+  }
+  console.log(e.keyCode);
+};
---
+++ js/snake/snake.js
@@ -0,0 +1,15 @@
+var ROWS = 50;
+var COLS = 50;
+var snake = [];
+var SNAKE_INITIAL_LENGTH = 5;
+
+function init() {
+  for (var i = 0; i < SNAKE_INITIAL_LENGTH; ++i) {
+    snake.push({
+      x: Math.floor(COLS / 2) - i,
+      y: Math.floor(ROWS / 2)
+    });
+  }
+}
+
+init();
